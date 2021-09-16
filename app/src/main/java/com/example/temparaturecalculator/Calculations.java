fc90c533d7dbba8d4c5d05f4b9055bd2a13ac784
Abeetha013 <abeethaasahara@gmail.com>
2021-09-17 02:06:38 +0530

    Temp_Cal

---
+++ app/src/main/java/com/example/temparaturecalculator/Calculations.java
@@ -0,0 +1,14 @@
+package com.example.temparaturecalculator;
+
+public class Calculations {
+
+    protected float  convertCelciusToFahrenheit(Float value) {
+        Float ans = (value * 9/5) + 32;
+        return ans;
+    }
+    protected float  convertFahrenheitToCelcius(Float value) {
+        Float ans = (value - 32) * 5/9;
+        return ans;
+    }
+
+}
